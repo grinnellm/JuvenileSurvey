3f2c1a86df2ba826f5207785afea4384440076f7
Grinnell <Matthew.Grinnell@dfo-mpo.gc.ca>
2023-11-29 12:27:35 -0800

    Specify packages

--- data/pars.R
+++ data/pars.R
@@ -3,9 +3,9 @@ library(readr)
 library(here)
 
 # Transect and station data
-ts_data <- read_csv(
+ts_data <- readr::read_csv(
   "Transect, Station, Date, Time
   1, 1, 6 Sept 2022, 19:00",
-  col_types = cols("d", "d", "c", "c")
+  col_types = readr::cols("d", "d", "c", "c")
 )
-save(ts_data, file = here("data", "ts_data.RData"))
+save(ts_data, file = here::here("data", "ts_data.RData"))
