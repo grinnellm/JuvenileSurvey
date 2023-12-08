8c9d5e8c730e67cc1dc0f0ec91aa1530ddb54823
Grinnell <Matthew.Grinnell@dfo-mpo.gc.ca>
2023-12-08 12:53:17 -0800

    Add stations info

--- data/pars.R
+++ data/pars.R
@@ -1,12 +1,9 @@
 # Transect and station data
 ts_data <- tibble::tribble(
-  ~Date,        ~Transect,
-  "2022-09-19", 1,
-  "2022-09-20", 3,
-  "2022-09-21", 6,
-  "2022-09-22", 12,
-  "2022-09-23", 10,
-  "2022-09-24", 44
+  ~Date,        ~Transect, ~Station,
+  "2021-09-19", 1,         "1,3",
+  "2021-09-20", 3,         "3,5",
+  "2021-09-21", 6,         "1,3"
 )
 # ts_data <- dplyr::mutate(ts_data, Date = lubridate::as_date(Date))
 save(ts_data, file = here::here("data", "ts_data.RData"))
